c9d7e03d1fcad539605110ffe61b18f04e3cd0af
Md_AsHrAFuL_IsLsM <121592381+ashraful009@users.noreply.github.com>
2025-04-29 09:23:38 +0600

    Create Heap Dynamic.py

---
+++ python/Heap Dynamic.py
@@ -0,0 +1,11 @@
+#heap dynamic array
+# This code demonstrates how to create a heap dynamic array in Python
+## The array is initialized with None, and then elements are appended to it.
+
+arr = []
+arr.append(10)
+arr.append(20)
+arr.append(30)
+arr.append(40)
+arr.append(50)
+print(arr)
